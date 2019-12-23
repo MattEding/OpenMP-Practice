954fc492a1758d81924ea0d2372d4626617af455
Matt Eding <matteding@gmail.com>
2019-12-23 14:26:39 -0800

    pi: pad omp array processing for caching

--- Exercises/Ex2-Pi/pi.cpp
+++ Exercises/Ex2-Pi/pi.cpp
@@ -5,6 +5,8 @@
 #include "omp.h"
 
 
+#define PAD 8
+
 double rect_area(const int idx, const double step) {
     double x = (idx + 0.5) * step;
     return 4.0 / (1.0 + x*x);
@@ -13,7 +15,7 @@ double rect_area(const int idx, const double step) {
 
 double calc_pi(const double num_steps, size_t &num_threads) {
     double step = 1.0 / static_cast<double>(num_steps);
-    std::vector<double> thread_sums(num_threads);
+    std::vector<double[PAD]> thread_sums(num_threads);
 
     omp_set_num_threads(num_threads);
     #pragma omp parallel
@@ -23,11 +25,12 @@ double calc_pi(const double num_steps, size_t &num_threads) {
             num_threads = omp_get_num_threads();
         }
         for (int i = id; i < num_steps; i += num_threads) {
-            thread_sums[id] += rect_area(i, step);
+            thread_sums[id][0] += rect_area(i, step);
         }
     }
 
-    double sum = std::accumulate(thread_sums.begin(), thread_sums.end(), 0.0);
+    double sum = std::accumulate(thread_sums.begin(), thread_sums.end(), 0.0,
+                                 [](const auto acc, const auto it){ return acc + it[0]; });
     return step * sum;
 }
 
@@ -47,8 +50,8 @@ T arg_to_var(const T default_val, const int argc, const char * const *argv) {
 
 
 int main(int argc, char **argv) {
-    long num_steps = arg_to_var<long>(100'000, argc, argv);
     size_t num_threads = arg_to_var<size_t>(4, argc, argv);
+    long num_steps = arg_to_var<long>(100'000, argc, argv);
 
     double t0 = omp_get_wtime();
     double pi = calc_pi(num_steps, num_threads);
